a233803f06f0bee24cfa1efdbf9aa1bedfb7f431
makspetukhov <petuchov@mail.ua>
2015-11-12 00:50:30 +0200

    new function

--- src/Connection.java
+++ src/Connection.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.Socket;
 
 public class Connection {
@@ -5,5 +6,21 @@ public class Connection {
 
    public Connection(Socket sockt){
         this.s=sockt;
+    }
+
+   public void sendNickHello(String nick) throws IOException {
+       s.getOutputStream().write(("ChatApp 2015 user "+nick+"\n").getBytes());
+   }
+   public void sendNickBusy(String nick) throws IOException {
+       s.getOutputStream().write(("ChatApp 2015 user " + nick + "busy\n").getBytes());
+   }
+   public void accept() throws IOException {
+       s.getOutputStream().write(("Accepted\n").getBytes());
+   }
+    public void reject() throws IOException {
+        s.getOutputStream().write(("Rejected\n").getBytes());
+    }
+    public void disconnect() throws IOException {
+        s.getOutputStream().write(("Disconnected\n").getBytes());
     }
 }
